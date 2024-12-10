e1d8aaabb7169729017302177d7656c9db9f7378
ashwini yedave <Ashwini Yedave>
2024-12-10 15:36:10 +0530

    college-spa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { DepartmentsComponent } from './departments/departments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route
+  { path: 'courses', component: CoursesComponent },
+  { path: 'faculty', component: FacultyComponent },
+  { path: 'departments', component: DepartmentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { DepartmentsComponent } from './departments/departments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    FacultyComponent,
+    DepartmentsComponent
   ],
   imports: [
     BrowserModule,
